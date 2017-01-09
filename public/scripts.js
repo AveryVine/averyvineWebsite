98a4dece2245f82686388deea286738fcb5c3219
Avery Vine <averyvine@icloud.com>
2017-01-08 19:47:37 -0500

    Fixed arrow tabs

--- public/scripts.js
+++ public/scripts.js
@@ -1,39 +1,36 @@
 $(document).ready(function() {
-  var tabIndex = {"skillTab" : 1, "workExperienceTab" : 1};
-
-  function collapseNavBar(navBar) {
-      var x = document.getElementById(navBar);
-      if (x.className.indexOf("w3-show") == -1) {
-          x.className += " w3-show";
-      } else {
-          x.className = x.className.replace(" w3-show", "");
-      }
-  }
-
-  function changeTab(n, tabClass) {
-    if (tabClass === "skillTab") showTab(tabIndex.skillTab += n, "skillTab");
-    else showTab(tabIndex.workExperienceTab += n, "workExperienceTab");
-  }
-
-  function showTab(n, tabClass) {
-    var index;
-    if (tabClass === "skillTab") index = tabIndex.skillTab;
-    else index = tabIndex.workExperienceTab;
-    var x = document.getElementsByClassName(tabClass);
-    console.log(x);
-    if (n > x.length) {index = 1}
-    if (n < 1) {index = x.length}
-    for (var i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    console.log("x[0]: " + x[1]);
-    x[index - 1].style.display = "block";
-    if (tabClass === "skillTab") tabIndex.skillTab = index;
-    else tabIndex.workExperienceTab = index;
-  }
-
-  showTab(tabIndex.skillTab, "skillTab");
-  showTab(tabIndex.workExperienceTab, "workExperienceTab");
-  console.log($('#grades').height());
   $('#gradesImage').height($('#grades').height());
 });
+
+function collapseNavBar(navBar) {
+    var x = document.getElementById(navBar);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function changeTab(n, tabClass) {
+  if (tabClass === "skillTab") showTab(tabIndex.skillTab += n, "skillTab");
+  else showTab(tabIndex.workExperienceTab += n, "workExperienceTab");
+}
+
+function showTab(n, tabClass) {
+  var index;
+  if (tabClass === "skillTab") index = tabIndex.skillTab;
+  else index = tabIndex.workExperienceTab;
+  var x = document.getElementsByClassName(tabClass);
+  if (n > x.length) {index = 1}
+  if (n < 1) {index = x.length}
+  for (var i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[index - 1].style.display = "block";
+  if (tabClass === "skillTab") tabIndex.skillTab = index;
+  else tabIndex.workExperienceTab = index;
+}
+
+var tabIndex = {"skillTab" : 1, "workExperienceTab" : 1};
+showTab(tabIndex.skillTab, "skillTab");
+showTab(tabIndex.workExperienceTab, "workExperienceTab");
