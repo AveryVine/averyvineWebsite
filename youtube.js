d1449e1e7efde8306ce2b2bb328777033b72b6f7
Avery Vine <averyvine@icloud.com>
2017-12-11 22:49:12 -0500

    added differentiation between channels and videos

--- youtube.js
+++ youtube.js
@@ -5,16 +5,25 @@ const Discord = require('discord.js');
 const $ = module.exports;
 
 var message = null;
-var youtubeVideo = null;
+var youtubeEntity = null;
 
-class YouTubeVideo {
-    constructor(title, url, channel, thumbnail) {
+class YouTubeEntity {
+    constructor(title, kind, url, channel, thumbnail) {
         this.title = title;
+        this.kind = kind;
         this.url = url;
         this.channel = channel;
         this.thumbnail = thumbnail;
 
-        console.log("Created YouTubeVideo: " + title + " (" + url + ")");
+        if (kind == "channel") {
+            console.log("Created YouTubeChannel: " + title + " (" + url + ")");
+        }
+        else if (kind == "video") {
+            console.log("Created YouTubeVideo: " + title + " (" + url + ")");
+        }
+        else {
+            console.log("Created YouTubeEntity: " + title + " (" + url + ")");
+        }
     }
 }
 
@@ -30,8 +39,8 @@ $.search = function (messageObject, searchInput) {
             console.log("Something went wrong!\n" + err);
             sendMessage("Something went wrong!");
         } else {
-            var video = results[0];
-            youtubeVideo = new YouTubeVideo(video.title, video.link, video.channelTitle, video.thumbnails.high.url);
+            var entity = results[0];
+            youtubeEntity = new YouTubeEntity(entity.title, entity.kind.replace('youtube#', ''), entity.link, entity.channelTitle, entity.thumbnails.high.url);
             sendEmbeddedMessage();
         }
     });
@@ -39,24 +48,33 @@ $.search = function (messageObject, searchInput) {
 
 function sendEmbeddedMessage() {
     var embedFields = [];
-    embedFields.push({
-        name: "Channel Name",
-        value: youtubeVideo.channel,
-        inline: true
-    })
-    embedFields.push({
-        name: "Video URL",
-        value: youtubeVideo.url,
-        inline: true
-    })
+    if (youtubeEntity.kind != "channel") {
+        embedFields.push({
+            name: "Channel Name",
+            value: youtubeEntity.channel,
+            inline: true
+        });
+        embedFields.push({
+            name: "Video URL",
+            value: youtubeEntity.url,
+            inline: true
+        });
+    }
+    else {
+        embedFields.push({
+            name: "Channel URL",
+            value: youtubeEntity.url,
+            inline: true
+        });
+    }
     var embedData = {
         author: {
-            name: youtubeVideo.title.toUpperCase(),
+            name: youtubeEntity.title.toUpperCase() + " - " + youtubeEntity.kind.charAt(0).toUpperCase() + youtubeEntity.kind.slice(1),
             icon_url: "https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697037-youtube-512.png"
         },
         color: 0xff0000,
         thumbnail: {
-            url: youtubeVideo.thumbnail
+            url: youtubeEntity.thumbnail
         },
         description: "------------------------------",
         fields: embedFields,
