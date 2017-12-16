0eb9df58deaf6eeffab15e4d91d362093c0ceac3
Avery Vine <averyvine@icloud.com>
2017-12-15 20:04:15 -0500

    groundwork for bans lookup help command

--- discordBot.js
+++ discordBot.js
@@ -13,6 +13,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	const commands = {
+		"help": function () { help(message) },
 		"summoner": function () { league.summoner(message, content) },
 		"bans": function () { league.bans(message, content) },
 		"youtube": function () { youtube.search(message, content) },
@@ -24,7 +25,7 @@ client.on('message', message => {
 			var content = message.content.slice(1);
 			console.log("Received message: " + content);
 			if (content.toLowerCase() === 'ping') {
-				message.channel.send('pong');
+				sendMessage('pong', message);
 			} else {
 				var command = content;
 				if (content.includes(' ')) {
@@ -47,6 +48,58 @@ function loadApiKeysFromProcess() {
 	apiKeys.twitchId = process.env.twitchId;
 
 	apiKeys.startBot = process.env.startBot;
+}
+
+function help(message) {
+	var embedFields = [];
+	embedFields.push({
+		name: "PING",
+		value: "Tests your connection to me!\n\t- Example: \"!ping\"",
+		inline: true
+	});
+	embedFields.push({
+		name: "HELP",
+		value: "Returns the list of accepted commands, with descriptions.\n\t- Example: \"!help\"",
+		inline: true
+	});
+	embedFields.push({
+		name: "SUMMONER",
+		value: "Provides details for the given summoner.\n\t- Required Input: the summoner name to look up\n\t- Example: \"!summoner Ace Damasos\"",
+		inline: true
+	});
+	embedFields.push({
+		name: "TWITCH",
+		value: "Searches Twitch for the given query.\n\t- Required Input: a query to search for\n\t- Example: \"!twitch imaqtpie\"",
+		inline: true
+	});
+	embedFields.push({
+		name: "YOUTUBE",
+		value: "Searches YouTube for the given query.\n\t- Required Input: a query to search for\n\t- Example: \"!youtube Thomas the Dank Engine\"",
+		inline: true
+	});
+    var embedData = {
+        author: {
+            name: "Hi, I'm Alfred.",
+            icon_url: "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        thumbnail: {
+            url: "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        description: "------------------------------\nI'm a bot that can help you with many things, most of which being related to League of Legends. My available commands are listed below. Have fun!",
+        fields: embedFields,
+        footer: {
+            icon_url: "https://cdn.discordapp.com/embed/avatars/0.png",
+            text: "I am a bot, beep boop."
+        },
+    };
+    embedData.timestamp = new Date();
+    var data = new Discord.RichEmbed(embedData);
+    sendMessage(data, message);
+}
+
+function sendMessage(reply, message) {
+    console.log("(" + new Date() + ") Sending message: " + reply);
+    message.channel.send(reply);
 }
 
 $.startBot = function () {
