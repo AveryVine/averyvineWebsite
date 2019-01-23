f58cc8d4f69e69266a0c6cc90f9bd977dcee668a
Avery Vine <avine@tripadvisor.com>
2019-01-23 17:41:31 -0500

    added tripadvisor hq as workplace, updated previous tripadvisor to past tense, rearranged projects, updated SSSC app project info and image, updated typewriter messages at top, various readability improvements

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   //konami code stuff
-  var konamiCounter = 0;
-  var konamiMessage = [
+  let konamiCounter = 0;
+  let konamiMessage = [
     "What, did you expect this to do something useful? P.S. I wouldn't do it again if I were you...",
     "If you keep doing this, things will end badly...",
     "You're playing with fire...",
     "This is not gonna end well for you...",
   ];
-  var konami = new Konami(function () {
+  let konami = new Konami(function () {
     if (konamiCounter < 2) {
       alert(konamiMessage[konamiCounter]);
       konamiCounter++;
@@ -25,7 +25,7 @@ $(document).ready(function () {
   $("a").on('click', function (event) {
     if (this.hash !== "") {
       event.preventDefault();
-      var offset = parseInt($(".navbar").css('height')) + 10;
+      let offset = parseInt($(".navbar").css('height')) + 10;
       $('html, body').animate({
         scrollTop: $(this.hash).offset().top - offset
       }, 800);
@@ -35,12 +35,12 @@ $(document).ready(function () {
   //add modal image code to all relevant images
   $(".figure").each(function () {
     $(this).on("click", function () {
-      var modal = $("#modal-display");
-      var modalImg = $("#modal-content");
-      var modalTxt = $("#modal-caption");
-      var img = $(this).find(".content-image");
-      var imgSrc = img.attr("src");
-      var imgTxt = $(this).find(".figure-caption").text();
+      let modal = $("#modal-display");
+      let modalImg = $("#modal-content");
+      let modalTxt = $("#modal-caption");
+      let img = $(this).find(".content-image");
+      let imgSrc = img.attr("src");
+      let imgTxt = $(this).find(".figure-caption").text();
 
       modalImg.attr("src", imgSrc);
       modalTxt.text(imgTxt);
@@ -55,14 +55,15 @@ $(document).ready(function () {
   //set up typeit
   const typeitPhrases = [
     'a co-op student',
-    'an application developer',
     'a black belt in karate',
-    'a mobile developer',
+    'an iOS developer',
     'an avid musician',
     'a full stack developer',
-    'a video game enthusiast'
+    'a video game enthusiast',
+    'a proud cat owner'
   ];
 
+  let phraseIndex = Math.floor(Math.random() * typeitPhrases.length);
   const headerTypeIt = new TypeIt('#header-typeit', {
     strings: [],
     breakLines: true,
@@ -70,11 +71,17 @@ $(document).ready(function () {
     autoStart: false,
     startDelay: 2000,
     afterString: function () {
-      headerTypeIt.pause(2000).delete().type(typeitPhrases[Math.floor(Math.random() * typeitPhrases.length)]);
+      console.log(phraseIndex);
+      let previousIndex = phraseIndex;
+      do {
+        phraseIndex = Math.floor(Math.random() * typeitPhrases.length);
+        console.log(phraseIndex);
+      } while (phraseIndex == previousIndex);
+      headerTypeIt.pause(2000).delete().type(typeitPhrases[phraseIndex]);
     }
   });
 
-  headerTypeIt.type(typeitPhrases[Math.floor(Math.random() * typeitPhrases.length)]);
+  headerTypeIt.type(typeitPhrases[phraseIndex]);
 
   //adjust parallax effect for devices that do not support "background-attachment: fixed"
   if (navigator.userAgent.toLowerCase().match(/(iphone|ipod|ipad)/)) {
@@ -89,12 +96,12 @@ let randomNumber = function (min, max) {
 
 //projects scripts
 const projectsToDisplay = 3;
-var firstVisibleProject = 1;
+let firstVisibleProject = 1;
 
 //runs when a user requests more projects
 let getProjects = function () {
   loadProjects();
-  var offset = parseInt($(".navbar").css('height')) + 10;
+  let offset = parseInt($(".navbar").css('height')) + 10;
   $('html, body').animate({
     scrollTop: $('#projects').offset().top - offset
   }, 800);
@@ -102,11 +109,11 @@ let getProjects = function () {
 
 //loads a set of up to {projectsToDisplay} projects (probably 3)
 let loadProjects = function () {
-  var projectsToShow = [];
-  var totalProjects = 0;
+  let projectsToShow = [];
+  let totalProjects = 0;
 
   //track which projects should be shown
-  for (var projectIndex = 1; $('#project' + projectIndex).length; projectIndex++) {
+  for (let projectIndex = 1; $('#project' + projectIndex).length; projectIndex++) {
     if (projectIndex >= firstVisibleProject && projectIndex < firstVisibleProject + projectsToDisplay) {
       projectsToShow.push($('#project' + projectIndex).attr('id'));
     }
@@ -114,8 +121,8 @@ let loadProjects = function () {
   }
 
   //hide all projects, then show tracked projects
-  for (var projectIndex = 1; $('#project' + projectIndex).length; projectIndex++) {
-    var project = $('#project' + projectIndex);
+  for (let projectIndex = 1; $('#project' + projectIndex).length; projectIndex++) {
+    let project = $('#project' + projectIndex);
     project.fadeOut(600, function () {
       if (projectsToShow.includes($(this).attr('id'))) {
         $(this).fadeIn(600);
@@ -162,9 +169,9 @@ window.onload = function () {
 
 //progress bar scripts
 window.onscroll = function () {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   $("#progress-bar").css("width", scrolled + "%");
   if (winScroll == 0 && $(".progress-container").is(":visible")) {
     $(".progress-container").fadeOut();
